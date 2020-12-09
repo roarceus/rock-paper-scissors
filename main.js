0ca54d1cf502773961d4f2095f5ef19ac0925634
Sohan <roarceus@users.noreply.github.com>
2020-12-09 17:54:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,86 @@
+const selectionButtons = document.querySelectorAll('[data-selection]');
+const playButtons = document.querySelector('.selections');
+const results = document.querySelector('.results')
+const restartContainer = document.querySelector('.restart');
+const restartBtn = document.querySelector('.restart-button');
+const historyContainer = document.querySelector('.history');
+const history = document.querySelector('.history-result');
+const yourScore = document.querySelector('[data-your-score]');
+const computerScore = document.querySelector('[data-computer-score]');
+
+
+playButtons.addEventListener('click', e => {
+    results.classList.remove('hidden');
+    restartContainer.classList.remove('hidden');
+    historyContainer.classList.remove('hidden');
+})
+
+const SELECTIONS = [
+    {
+        name: 'rock',
+        emoji: '🗿',
+        beats: 'scissors'
+    },
+    {
+        name: 'paper',
+        emoji: '🧻',
+        beats: 'rock'
+    },
+    {
+        name: 'scissors',
+        emoji: '✂️',
+        beats: 'paper'
+    }
+];
+
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+        const selectionName = selectionButton.dataset.selection;
+        const selection = SELECTIONS.find(selection => selection.name === selectionName);
+        makeSelection(selection)
+    })
+});
+
+function makeSelection(selection) {
+    const computerSelection = randomSelection();
+    const yourWinner = isWinner(selection, computerSelection);
+    const computerWinner = isWinner(computerSelection, selection);
+    addSelectionResult(computerSelection, computerWinner);
+    addSelectionResult(selection, yourWinner);
+    if (yourWinner) incrementScore(yourScore)
+    if(computerWinner) incrementScore(computerScore)
+}
+
+function addSelectionResult(selection, winner) {
+    const div = document.createElement('div');
+    div.innerText = selection.emoji;
+    div.classList.add('result-selection');
+    if (winner) div.classList.add('winner');
+    history.after(div)
+}
+
+function isWinner(selection, opponentSelection) {
+    return selection.beats === opponentSelection.name;
+}
+
+function randomSelection() {
+    const randomIndex = Math.floor(Math.random() * SELECTIONS.length);
+    return SELECTIONS[randomIndex];
+}
+
+function incrementScore(score) {
+    score.innerText = parseInt(score.innerText) + 1;
+}
+
+function resetScore(score) {
+    score.innerText = parseInt(score.innerText) - parseInt(score.innerText);
+}
+
+restartBtn.addEventListener('click', e => {
+    resetScore(yourScore);
+    resetScore(computerScore);
+    results.classList.add('hidden');
+    historyContainer.classList.add('hidden');
+    // historyContainer.innerText = "";
+    restartContainer.classList.add('hidden');
+})
